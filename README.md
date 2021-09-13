35129feafea798a712cfa8446b298f317d22d35a
ygj6 <yuangongji@foxmail.com>
2021-09-13 09:32:16 +0800

    create bug report project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# bug-report-ctx
+# Vite Vue Starter
+
+This is a project template using [Vite](https://vitejs.dev/). It requires [Node.js](https://nodejs.org) v12+.
+
+To start:
+
+```sh
+npm install
+npm run dev
+
+# if using yarn:
+yarn
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+const _ = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg" ref="input">
+  <!-- in dev mode will report 'Uncaught TypeError: Cannot read property '__isScriptSetup' of undefined',work fine in prod mode -->
+  <button @click="$refs.input.focus()">
+    Focus $refs.input
+  </button>
+</template>
